fe009c849432876470b1ac021d4b0434dc505c69
Raz4core <mail@test.de>
2022-01-18 11:09:26 +0100

    Fallunterscheidung /Case switch1

--- script_01.js
+++ script_01.js
@@ -2,23 +2,23 @@
 
 // Entscheidungsstrukturen | control flow
 
-//Deklaration + Assigment
-const ageJohn = 30;
-const ageMark = 30;
+// //Deklaration + Assigment
+// const ageJohn = 30;
+// const ageMark = 30;
 
-//Deklaration
-let isJohnOlder, isJohnEqual;
+// //Deklaration
+// let isJohnOlder, isJohnEqual;
 
-//Test Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+// //Test Logische Aussage
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
-//Ausgabe
-console.log("ageJohn:" + ageJohn);
-console.log("ageMark:" + ageMark);
-console.log("isJohnOlder:" + isJohnOlder);
-console.log("isJohnEqual:" + isJohnEqual);
-console.log("--------------------");
+// //Ausgabe
+// console.log("ageJohn:" + ageJohn);
+// console.log("ageMark:" + ageMark);
+// console.log("isJohnOlder:" + isJohnOlder);
+// console.log("isJohnEqual:" + isJohnEqual);
+// console.log("--------------------");
 
 /** IF **/
 // TINA --> There is no alternative
@@ -48,21 +48,58 @@ console.log("--------------------");
 
 /*****IF - Else IF */
 
-if (isJohnOlder)
- {
-      //Ja-Zweig / true 
-      console.log("John ist Älter.");  
- }
-   //1.Alternative
-   else if(isJohnEqual)  
-   {
-       console.log("John ist Älter.");
-   }
-     //evtl. weitere Alternativen
-      else
+// if (isJohnOlder)
+//  {
+//       //Ja-Zweig / true 
+//       console.log("John ist Älter.");  
+//  }
+//    //1.Alternative
+//    else if(isJohnEqual)  
+//    {
+//        console.log("John ist Älter.");
+//    }
+//      //evtl. weitere Alternativen
+//       else
      
-     {
+//      {
 
-      //Nein-Zweig / false 
-      console.log("John ist Jünger.");
-     }
+//       //Nein-Zweig / false 
+//       console.log("John ist Jünger.");
+//      }
+
+/**Fallunterwscheidung /Case switch 1 */
+
+const firstName = "Jane";
+let job;
+
+job = "driver";  //.. fährt TAXI!/ UBER
+job = "diver";   //.. taucht im Rhein!
+job = "artist";  //.. malt ein Bild!
+job = "pilot" ;  //.. macht etwas anderes! --> default
+job = "teacher"; //.. unterrichtet!
+job = "instructor"; //.. unterrichtet!
+
+switch (job) {
+    case "driver":  // Test auf ==
+        console.log(firstName +" fährt TAXI!");
+        break;
+    case "diver":   // Test auf ==
+        console.log(firstName +" taucht im Rhein!");
+        break;
+    case "artist":  // Test auf ==
+        console.log(firstName +" malt ein Bild!");
+        break;
+    case "teacher": // Test auf ==
+        console.log(firstName +" unterrichtet!");
+        break;
+    case "instructor": // Test auf ==
+        console.log(firstName +" unterrichtet!");
+        break;
+
+    
+    
+    
+    default: // Black Swan !!!
+        console.log(firstName + "macht etwas anderes!");
+        break;
+}
